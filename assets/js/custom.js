564250394b9f32f32e67897a475911b528464cfd
xxlilspoonxx <58666669+xxlilspoonxx@users.noreply.github.com>
2020-09-27 20:22:26 -0500

    Klean Trial 3

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+//Intialize Slider
+var slider = new Swiper('#hero', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed:1000
+});
+
+//Intialize Testamonials
+var testimonials_slider = new Swiper('#testimonials_slider', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    parallax: true,
+    speed:1000
+});
